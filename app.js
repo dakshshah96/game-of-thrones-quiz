37d443f3c2a328e882fd3a5908d866c9cc330567
Daksh Shah <dakshshah96@gmail.com>
2017-04-03 22:30:53 +0530

    Show question number

---
+++ app.js
@@ -0,0 +1,114 @@
+var quiz = {
+    currentQuestion: 0,
+    questions: [{
+        question: "What is the name of Jon Snow's direwolf?",
+        answers: [
+            "Ghost",
+            "Grey Wind",
+            "Graham",
+            "Ricky"
+        ],
+        correctAnswer: 0
+    }, {
+        question: "What's the name of Lysa Arryn's way-too-old-to-be-nursing son?",
+        answers: [
+            "Ned",
+            "Robin",
+            "Edmore",
+            "Tyrion"
+        ],
+        correctAnswer: 1
+    }, {
+        question: "What's the name of the explosive that gave the Lannisters the edge in the Battle of Blackwater?",
+        answers: [
+            "Wildfire",
+            "Dragonfire",
+            "Godsfire",
+            "Pantsonfire"
+        ],
+        correctAnswer: 0
+    }, {
+        question: "How is \"The Queen Of Thorns\" more commonly known?",
+        answers: [
+            "Cersei Lannister",
+            "Margaery Tyrell",
+            "Olenna Tyrell",
+            "E Jarvis Thribb"
+        ],
+        correctAnswer: 2
+    }, {
+        question: "What is Pycelle's official title in the Red Keep?",
+        answers: [
+            "Master of Coin",
+            "Games Maester",
+            "Lord Commander of the Kingsguard",
+            "Grand Maester"
+        ],
+        correctAnswer: 3
+    }, {
+        question: "Which Lannister song signalled doom at the Red Wedding?",
+        answers: [
+            "The Rains Of Castermere",
+            "A Golden Crown",
+            "The Assassin's Dagger",
+            "Tales Of Topographic Oceans"
+        ],
+        correctAnswer: 0
+    }, {
+        question: "What piece of fencing advice did Jon Snow give to Arya Stark? \"Stick them with the…\"",
+        answers: [
+            "\"…Prickly end\"",
+            "\"…Sharp end\"",
+            "\"…Futtocks End\"",
+            "\"…Pointy end\""
+        ],
+        correctAnswer: 3
+    }, {
+        question: "Who said, \"Some day I'm gonna put a sword through your eye and out the back of your skull\"?",
+        answers: [
+            "Theon Greyjoy",
+            "Shae",
+            "Arya Stark",
+            "The Mountain"
+        ],
+        correctAnswer: 2
+    }, {
+        question: "Who was burned alive on Drogo's funeral pyre?",
+        answers: [
+            "Khal Drogo",
+            "Mirri Maz Duur",
+            "Maris Piper",
+            "Septa Mordane"
+        ],
+        correctAnswer: 1
+    }, {
+        question: "Who said, \"If you ever call me sister again, I'll have you strangled in your sleep\"?",
+        answers: [
+            "Annie Lennox",
+            "Meg White",
+            "Catelyn Stark",
+            "Cersei Lannister"
+        ],
+        correctAnswer: 3
+    }],
+    userAnswer: []
+};
+
+function generateQuestion(quiz) {
+
+}
+
+$(function() {
+
+    $('.startButton').click(function() {
+        $("#question").removeAttr('hidden');
+    });
+
+    if (typeof quiz.questions[0] != "undefined") {
+        $("#question")
+            .find("p").html('<span class="questionNumber">' + quiz.currentQuestion + 1 + " of 10:" + "</span> " + quiz.questions[0].question).end()
+            .find("ul li").each(function(index, element) {
+                $(this).find("span").text(quiz.questions[0].answers[index]);
+            });
+    }
+});
